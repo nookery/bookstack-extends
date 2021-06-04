3bd9a97b049e027ef2a6d077d327a582c87dde12
Nookery <nooks@qq.com>
2021-06-04 22:53:19 +0800

    Update BaseServiceProvider.php

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -17,16 +17,39 @@ class BaseServiceProvider extends ServiceProvider
         // 注册视图
         $this->loadViewsFrom(base_path('extends/views'), 'extends');
 
+        $setting = setting()->get('app-custom-head');
+        $settingChanged = false;
+        $cssPath = public_path('custom.css');
+        $jsPath = public_path('custom.js')
+
         // 加载自定义CSS
-        $appCustomHeadSetting = setting()->get('app-custom-head');
-        if (file_exists(public_path('custom.css')) && !strstr($appCustomHeadSetting, 'custom.css')) {
-            setting()->put('app-custom-head', $appCustomHeadSetting.PHP_EOL.'<link rel="stylesheet" href="/custom.css">');
+        $cssSeting = '<link rel="stylesheet" href="/custom.css">';
+        if (file_exists($cssPath) && !strstr($setting, 'custom.css')) {
+            $setting = $setting.PHP_EOL.$cssSeting;
+            $settingChanged = true;
         }
         
         // 加载自定义JS
-        $appCustomHeadSetting = setting()->get('app-custom-head');
-        if (file_exists(public_path('custom.js')) && !strstr($appCustomHeadSetting, 'custom.js')) {
-            setting()->put('app-custom-head', $appCustomHeadSetting.PHP_EOL.'<script src="/custom.js"></script>');
+        $jsSeting = '<script src="/custom.js"></script>';
+        if (file_exists($jsPath) && !strstr($setting, 'custom.js')) {
+            $setting = $setting.PHP_EOL.$jsSeting;
+            $settingChanged = true;
+        }
+
+        // 如果custom.css文件不存在，删除设置
+        if (!file_exists($cssPath) && strstr($setting, 'custom.css')) {
+            $setting = str_replace($cssSeting, '', $setting);
+            $settingChanged = true;
+        }
+
+        // 如果custom.js文件不存在，删除设置
+        if (!file_exists(jspath) && strstr($setting, 'custom.js')) {
+            $setting = str_replace($jsSeting, '', $setting);
+            $settingChanged = true;
+        }
+
+        if ($settingChanged) {
+            setting()->put('app-custom-head', $setting);
         }
     }
 }
