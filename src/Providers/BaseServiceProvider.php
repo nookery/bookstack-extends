c151c8b62085a9bfa10ca5a9c10dcd5b41fce619
nookery <nookery@outlook.com>
2021-06-04 15:39:51 +0800

    Initial commit

---
+++ src/Providers/BaseServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nookery\BootstackExtends\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        // 加载路由
+        $this->loadRoutesFrom(base_path('extends/routes.php'));
+
+        // 注册视图
+        $this->loadViewsFrom(base_path('extends/views'), 'extends');
+    }
+}
