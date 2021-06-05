d4e830f0a85316823c903852c74e465a775db458
Nookery <nooks@qq.com>
2021-06-05 22:26:18 +0800

    Update BaseServiceProvider.php

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -19,32 +19,56 @@ class BaseServiceProvider extends ServiceProvider
 
         $setting = setting()->get('app-custom-head');
         $settingChanged = false;
-        $cssPath = public_path('custom.css');
-        $jsPath = public_path('custom.js');
+
+        $cssLink = '/custom/custom.css';
+        $jsLink = '/custom/custom.js';
+        $faviconLink = '/custom/favicon.ico';
+        $cssPath = public_path($cssLink);
+        $jsPath = public_path($jsLink);
+        $faviconPath = public_path($faviconLink);
 
         // 加载自定义CSS
-        $cssSeting = '<link rel="stylesheet" href="/custom.css">';
-        if (file_exists($cssPath) && !strstr($setting, 'custom.css')) {
+        $cssSeting = '<link rel="stylesheet" href="'.$cssLink.'">';
+        if (file_exists($cssPath) && !strstr($setting, $cssLink)) {
             $setting = $setting.PHP_EOL.$cssSeting;
             $settingChanged = true;
         }
         
         // 加载自定义JS
-        $jsSeting = '<script src="/custom.js"></script>';
-        if (file_exists($jsPath) && !strstr($setting, 'custom.js')) {
+        $jsSeting = '<script src="'.$jsLink.'"></script>';
+        if (file_exists($jsPath) && !strstr($setting, $jsLink)) {
             $setting = $setting.PHP_EOL.$jsSeting;
             $settingChanged = true;
         }
 
+        // 加载自定义favicon.ico
+        $faviconSetting = '<link rel="shortcut icon" href="'.$faviconLink.'"/>';
+        if (file_exists($faviconPath) && !strstr($setting, $faviconLink)) {
+            $setting = $setting.PHP_EOL.$faviconLink;
+            $settingChanged = true;
+        }
+
         // 如果custom.css文件不存在，删除设置
-        if (!file_exists($cssPath) && strstr($setting, 'custom.css')) {
+        if (!file_exists($cssPath) && strstr($setting, $cssLink)) {
             $setting = str_replace($cssSeting, '', $setting);
             $settingChanged = true;
         }
 
         // 如果custom.js文件不存在，删除设置
-        if (!file_exists($jsPath) && strstr($setting, 'custom.js')) {
+        if (!file_exists($jsPath) && strstr($setting, $jsLink)) {
             $setting = str_replace($jsSeting, '', $setting);
+            $settingChanged = true;
+        }
+
+        // 如果custom.js文件不存在，删除设置
+        if (!file_exists($jsPath) && strstr($setting, $jsLink)) {
+            $setting = str_replace($jsSeting, '', $setting);
+            $settingChanged = true;
+        }
+
+        // 如果favicon.ico文件不存在，删除设置
+        if (!file_exists($faviconPath) && strstr($setting, $faviconLink)) {
+            $setting = str_replace($faviconSetting, '', $setting);
             $settingChanged = true;
         }
 
